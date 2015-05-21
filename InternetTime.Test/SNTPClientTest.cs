f501d628b8d9e965667ae989fba8f9a932a22d54
Marcel Valdez <marcel.valdez@live.com>
2015-05-20 20:45:42 -0400

    Crated mono compatible version

---
+++ InternetTime.Test/SNTPClientTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using InternetTime;
+using NUnit.Framework;
+
+namespace InternetTimeTest
+{
+  [TestFixture]
+  public class SNTPClientTest
+  {
+    [Test]
+    public void TestConnect()
+    {
+      // Arrange
+      SNTPClient client = new SNTPClient("time.nist.gov");
+      // Act 
+      client.Connect(false);
+      // Assert
+      DateTime now = DateTime.Now;
+      DateTime serverTime = client.InternetTime;
+      Assert.That(serverTime, Is.LessThan(now.AddSeconds(0)));
+      Assert.That(serverTime, Is.GreaterThan(now.AddSeconds(0)));
+    }
+  }
+}
